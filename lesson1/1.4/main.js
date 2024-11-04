7b2e616bd15c1a3c65d983ba5c24f51650e30efb
s3r3nd1pity <sasaserbinena@gmail.com>
2024-11-04 00:02:50 -0500

    [add]:[file]:main.js

--- lesson1/1.4/main.js
+++ lesson1/1.4/main.js
@@ -1,7 +1,6 @@
 let firstName = prompt("Введіть ваше ім'я:");
 let midleName = prompt("Введіть ваше по-батькові:");
 let age = prompt("Введіть ваш вік:");
-
 console.log( "Ім'я:", firstName);
 console.log("По-батькові:", midleName);
 console.log("Вік:", age);
