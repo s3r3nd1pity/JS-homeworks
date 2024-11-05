2356af9a3caf9457c2f3e53ea5f06884a7ee619e
s3r3nd1pity <sasaserbinena@gmail.com>
2024-11-04 23:18:13 -0500

    [add]:[file]: #LARqoUj5I.html, main.js

---
+++ lessson2/2.2/main.js
@@ -0,0 +1,16 @@
+let a = {
+    title: 'Sapiens: A Brief History of Humankind',
+    pageCount: 443,
+    genre: 'Non-fiction, History',
+}
+let b = {
+    title: '1984',
+    pageCount: 328,
+    genre: 'Dystopian, Science Fiction',
+}
+let c = {
+    title: 'The Great Gatsby',
+    pageCount: 180,
+    genre: 'Fiction, Classic',
+}
+console.log(a,b,c)
