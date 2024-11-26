250e23a2ed59984891f42a417de166657f8a942f
s3r3nd1pity <sasaserbinena@gmail.com>
2024-11-25 23:43:14 -0500

    add homeworks

---
+++ resolve/12.js
@@ -0,0 +1,48 @@
+// let users = [
+//     { id: 1, name: "Alice", age: 25 },
+//     { id: 2, name: "Bob", age: 30 },
+//     { id: 3, name: "Charlie", age: 22 }
+// ];
+// function writer(){
+//     for (const user of users) {
+// document.write(user.id,', ', user.name,`, `, user.age)
+//     }
+//                 }
+//
+//
+//
+// document.write(`<ul>`)
+// for (const user of users) {
+//     document.write(`<li>`)
+//     document.write(user.id,', ', user.name,`, `, user.age)
+//     document.write(`</<li>`)
+//
+// }
+// document.write(`</ul>`)
+
+// let arr =[-5,1,2,0,-100,10]
+// function min(arr) {
+//     let minimum = arr[0]
+//
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i]< minimum){
+//             minimum= arr[i]
+//         }
+//     }
+//     return minimum
+//
+// }
+//
+// console.log(min(arr));
+
+// function swap(arr,index1,index2){
+//     let im = arr[index1]
+//
+//     if (index1<arr.length && index2<arr.length){
+//         arr[index1]=arr[index2]
+//         arr[index2]=im
+//     }
+//     return arr
+// }
+//
+// console.log(swap([11, 22, 33, 44], 1, 3));
